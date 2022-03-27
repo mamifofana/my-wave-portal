a2cbb7a7123a16e49e80dd4967cb639c0d5bc3b9
mamifofana <mamifofana.innovator@gmail.com>
2022-03-27 12:37:56 +0200

    store data functions wave and get wave and update runjs

--- scripts/run.js
+++ scripts/run.js
@@ -2,18 +2,28 @@ const header = require("hardhat");
 
 
 const main = async() =>{
+    const [owner, randomPerson] = await header.ethers.getSigners();
     const waveContractFactory = await header.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
+
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address)
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
 };
 
 
 const runMain = async () => {
     try {
         await main();
-        process.exit();
-        process.exit(0); // exit Node process without error
+        process.exit(0);
     } catch (error) {
         console.log(error);
         process.exit(1); // exit Node process while indicating ' uncaught fatal exception' error 
